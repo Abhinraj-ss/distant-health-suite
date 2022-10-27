3116f942fc06bc601582c4f79b48922aeb0911cb
Abhinraj-ss <abhinrajss18@gmail.com>
2022-10-27 08:11:33 +0530

    First commit

---
+++ suite/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Distatnt Health suite</h1>
+      <p>Loading.........</p>
+    </div>
+    
+  )
+}
+
+export default App
